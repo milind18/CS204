b2e9094faa6f3d549a4cb78afca37ad275e5975f
milind18 <53634719+milind18@users.noreply.github.com>
2019-11-17 12:37:13 +0530

    Update lab12.cpp

--- LAB12/lab12.cpp
+++ LAB12/lab12.cpp
@@ -1,127 +1,59 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class UnionFind 
+#define M 1000000007
+#define ll long long
+#define REP(i,a,b) for(ll i=a;i<b;i++)
+#define REPI(i,a,b) for(ll i=b-1;i>=a;i--)
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define V(a) vector<a>
+
+ll fn(ll x,ll rn[])
 {
-public:
-    UnionFind(int n) 
-   	{
-        size = n;
-        sets = new int[size];
-        rank = new int[size];
-        for(int i=0; i < size; i++)
-        {
-            sets[i] = i; rank[i] = 0;
-        }
-    }
-
-    ~UnionFind()
-    {
-        delete [] sets;
-        delete [] rank;
-    }
-    
-    int find(int s) 
-    {
-        assert(s >= 0 && s < size);
-        vector<int> A;
-        while(s != sets[s])
-        {
-            A.push_back(s);
-            s = sets[s];
-        }
-        for(int i=0; i < A.size(); i++) 
-        	sets[A[i]] = s;
-        return s;
-    }
-    bool merge(int s1, int s2) 
-    {
-        assert(s1 >= 0 && s1 < size);
-        assert(s2 >= 0 && s2 < size);
-        int s1_rep = find(s1);
-
-        int s2_rep = find(s2);
-
-        if(s1_rep == s2_rep) return false;
-
-        if(rank[s1_rep] > rank[s2_rep])
-        {
-            sets[s2_rep] = s1_rep;
-        }
-        else if(rank[s1_rep] < rank[s2_rep])
-        {
-            sets[s1_rep] = s2_rep;
-        } 
-        else 
-        {    
-            sets[s2_rep] = s1_rep;
-            rank[s1_rep] += 1;
-        }
-
-        return true;    
-    }
-    void print_sets()
-    {
-        for(int i=0; i < size; i++) cout << find(i) << " ";
-        cout << endl;
-    }
-private:
-    int size = 0;
-    int* sets;  
-    int* rank;
-};
-
-struct edge
-{
-	int u;
-	int v;
-	long double w;
-};
-bool compare(edge a,edge b)
-{
-	if(a.w>b.w)
-		return true;
-	else
-		return false;
+    if(x==rn[x]) return x;
+    else return rn[x]=fn(rn[x],rn);
 }
+
+ll un(ll x,ll y,ll rn[],ll sz[])
+{
+    x=fn(x,rn);
+    y=fn(y,rn);
+    if(x==y) return false;
+    if(sz[x]<sz[y]) swap(x,y);
+    sz[x]+=sz[y];
+    rn[y]=x;
+    return true;
+}
+
 int main()
 {
-	int n,m;
-	cin>>n>>m;
-	vector<edge>edges(m);
-	int u,v,w;
-	for(int i=0;i<m;i++)
-	{
-		cin>>u>>v>>w;
-		edges[i].u=u;
-		edges[i].v=v;
-		edges[i].w=w;
+    ll n,m;
+    cin>>n>>m;
+    vector<pair<ll,pair<ll,ll>>> v;
+    REP(i,0,m)
+    {
+        ll t1,t2,t3;
+        cin>>t1>>t2>>t3;
+        t1--;t2--;
+        v.PB(MP(t3,MP(t1,t2)));
+    }
+    sort(v.begin(),v.end());
+    ll ans=1;
+    ll rn[n];
+    ll sz[n];
+    REP(i,0,n)
+    {
+        rn[i]=i;
+        sz[i]=1;
+    }
+    REPI(i,0,m)
+    {
+        if(un((v[i].S).F,(v[i].S).S,rn,sz)) ans*=(v[i].F)%M;
+        ans%=M;
+    }
+    cout<<ans;
 
-	}
-	sort(edges.begin(),edges.end(),compare);
-	for(int i=0;i<edges.size();i++)
-	{
-		cout<<edges[i].w<<endl;
-	}
-
-	UnionFind connect(n);
-	long double ans=0;
-	int count=0;
-
-	for(int i=0;i<edges.size();i++)
-	{
-		if(connect.find(edges[i].u)==connect.find(edges[i].v))
-		{
-			continue;
-		}
-		else
-		{
-			ans+=edges[i].w;
-			connect.merge(edges[i].v,edges[i].u);
-			count++;
-		}
-	}
-	connect.print_sets();
-	cout<<count;
-
-	return 0;
+    return 0;
 }
